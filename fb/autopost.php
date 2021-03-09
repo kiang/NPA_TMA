c3257edb763b1ada7263a61444b9da12f8210fc3
kiang <kiange@gmail.com>
2021-03-09 21:25:37 +0800

    first draft to post at fb

---
+++ fb/autopost.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * ref
+ * 1. https://adamboother.com/blog/automatically-posting-to-a-facebook-page-using-the-facebook-sdk-v5-for-php-facebook-api/
+ * 2. https://phppot.com/php/publishing-multi-photo-stories-to-facebook-using-php-sdk/
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+$config = require __DIR__ . '/config.php';
+
+$fb = new Facebook\Facebook([
+    'app_id' => $config['app_id'],
+    'app_secret' => $config['app_secret'],
+    'default_graph_version' => 'v2.2',
+]);
+
+$photos = [
+    __DIR__ . '/img_to_post.png',
+    __DIR__ . '/img_to_post2.png',
+];
+$media = [];
+foreach($photos AS $photo) {
+    $response = $fb->post('/me/photos', [
+        'source' => $fb->fileToUpload($photo),
+        'published' => false,
+    ], $config['token']);
+    $media[] = ['media_fbid' => $response->getDecodedBody()['id']];
+}
+
+//Post property to Facebook
+$linkData = [
+    'message' => 'NPA TMA testing',
+    'attached_media' => $media,
+];
+
+try {
+    $response = $fb->post('/me/feed', $linkData, $config['token']);
+} catch (Facebook\Exceptions\FacebookResponseException $e) {
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch (Facebook\Exceptions\FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+$graphNode = $response->getGraphNode();
