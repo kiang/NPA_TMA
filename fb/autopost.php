ba1488f258a1b95e93af92d840dec00430e01c67
kiang <kiange@gmail.com>
2021-03-09 21:39:34 +0800

    full sample to post at fb

--- fb/autopost.php
+++ fb/autopost.php
@@ -22,21 +22,43 @@ $fb = new Facebook\Facebook([
 ]);
 
 $photos = [
-    __DIR__ . '/img_to_post.png',
-    __DIR__ . '/img_to_post2.png',
+    __DIR__ . '/tmp/img_to_post1.png',
+    __DIR__ . '/tmp/img_to_post2.png',
+    __DIR__ . '/tmp/img_to_post3.png',
+    __DIR__ . '/tmp/img_to_post4.png',
 ];
 $media = [];
 foreach($photos AS $photo) {
-    $response = $fb->post('/me/photos', [
-        'source' => $fb->fileToUpload($photo),
-        'published' => false,
-    ], $config['token']);
+    try {
+        $response = $fb->post('/me/photos', [
+            'message' => '',
+            'source' => $fb->fileToUpload($photo),
+            'published' => false,
+        ], $config['token']);    
+    } catch (Facebook\Exceptions\FacebookResponseException $e) {
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit();
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit();
+    }
     $media[] = ['media_fbid' => $response->getDecodedBody()['id']];
 }
 
+$message = '國內交通事故通報 2021 | 02-22 ~ 02-28';
+$message .= "\n\n❗新增 31 例死亡，桃園市 6 例、新竹縣 5 例、高雄市 5 例❗";
+$message .= "\n⭐全部報表 - https://github.com/kiang/NPA_TMA/tree/master/report/";
+$message .= "\n⭐地圖 - https://kiang.github.io/NPA_TMA/";
+$message .= "\n\n✏警政署週一(3/8)更新 A1 即時交通事故資料，共新增 75 筆事故資料，其中 02-22 ~ 02-28 新增 31 例死亡。";
+$message .= "\n\n✏02-22 ~ 02-28 新增 31 例死亡、 22 例受傷與 29 起事故";
+$message .= "\n✏02-15 ~ 02-21 新增 41 例死亡、 676 例受傷與 496 起事故";
+$message .= "\n✏02-08 ~ 02-14 新增 43 例死亡、 2872 例受傷與 2166 起事故";
+$message .= "\n✏02-01 ~ 02-07 新增 48 例死亡、 4844 例受傷與 3625 起事故";
+$message .= "\n\n#交通安全最前線 #謝謝辛苦的警察人員";
+
 //Post property to Facebook
 $linkData = [
-    'message' => 'NPA TMA testing',
+    'message' => $message,
     'attached_media' => $media,
 ];
 
