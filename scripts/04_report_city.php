6dd532856511d5772efc2e85ff4baca8971d7eb8
kiang <kiange@gmail.com>
2022-01-03 16:49:35 +0800

    use newer data

--- scripts/04_report_city.php
+++ scripts/04_report_city.php
@@ -2,7 +2,7 @@
 
 $basePath = dirname(__DIR__);
 
-$roadsaftyFile = '/home/kiang/public_html/roadsafety.tw/raw/GetCitiesAreaAccDataStatistics/ALL/110_07.json';
+$roadsaftyFile = '/home/kiang/public_html/roadsafety.tw/raw/GetCitiesAreaAccDataStatistics/ALL/110_10.json';
 $baseCount = 0;
 if(file_exists($roadsaftyFile)) {
     $json = json_decode(file_get_contents($roadsaftyFile), true);
