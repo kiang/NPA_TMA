25d3f59850e86f4d1dd2fd583d727d579c39010a
kiang <kiange@gmail.com>
2022-12-28 21:14:57 +0800

    get new raw

--- scripts/01_fetch.php
+++ scripts/01_fetch.php
@@ -1,14 +1,20 @@
 <?php
 
-// A1 - https://data.moi.gov.tw/MoiOD/Data/DataDetail.aspx?oid=F4077949-50CC-4640-8114-79958CC8BBEA
-// A2 - https://data.moi.gov.tw/MoiOD/Data/DataDetail.aspx?oid=F713DBFE-7432-4401-B5C0-1C07A8F5B1FB
+// A1 - https://data.gov.tw/dataset/12818
+// A2 - https://data.gov.tw/dataset/13139
 $basePath = dirname(__DIR__);
 require $basePath . '/vendor/autoload.php';
 
 use Goutte\Client;
 
+$y = date('Y');
+$path = $basePath . '/data/' . $y;
+if (!file_exists($path)) {
+    mkdir($path, 0777, true);
+}
+
 $client = new Client();
-$client->request('GET', 'https://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=01987403-8634-4E3F-B626-E7777014AE43');
-file_put_contents($basePath . '/raw/a1.json', $client->getResponse()->getContent());
-$client->request('GET', 'https://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=35011243-38FD-42BF-9073-A3DE6A696532');
-file_put_contents($basePath . '/raw/a2.json', $client->getResponse()->getContent());
+$client->request('GET', 'https://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=402E554F-10E7-42C9-BAAF-DF7C431E3F18');
+file_put_contents($path . '/a1.csv', $client->getResponse()->getContent());
+$client->request('GET', 'https://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=1B920938-26EA-4A7C-BEFD-1E6503AA5D5E');
+file_put_contents($path . '/a2.csv', $client->getResponse()->getContent());
