b1501b5f4ec5bbc28597d64abdb0d1c92fe86e50
kiang <kiange@gmail.com>
2023-02-14 07:59:52 +0800

    to count all cities

--- scripts/03_calc.php
+++ scripts/03_calc.php
@@ -2,43 +2,54 @@
 
 $basePath = dirname(__DIR__);
 $count = $vehicles = [];
-$fh = fopen($basePath . '/data/a1.csv', 'r');
+$fh = fopen($basePath . '/data/2023/a1.csv', 'r');
 $head = fgetcsv($fh, 2048);
-while($line = fgetcsv($fh, 2048)) {
-    $parts = explode(';', $line[2]);
-    foreach($parts AS $part) {
-        $d = preg_split('/([0-9]+)/', $part, -1, PREG_SPLIT_DELIM_CAPTURE);
-        if(!isset($count[$d[0]])) {
-            $count[$d[0]] = 0;
+$pool = [
+    'total' => [],
+];
+while ($line = fgetcsv($fh, 2048)) {
+    $data = array_combine($head, $line);
+    if ($data['當事者順位'] == 1) {
+        $parts = explode(';', $data['死亡受傷人數']);
+        $count = [];
+        foreach ($parts as $v) {
+            $count[mb_substr($v, 0, 2, 'utf-8')] = intval(mb_substr($v, 2, null, 'utf-8'));
         }
-        $count[$d[0]] += $d[1];
-    }
-    $parts = explode(';', $line[3]);
-    foreach($parts AS $part) {
-        if(!isset($vehicles[$part])) {
-            $vehicles[$part] = 0;
+        if (!isset($pool[$data['處理單位名稱警局層']])) {
+            $pool[$data['處理單位名稱警局層']] = [];
         }
-        ++$vehicles[$part];
+        foreach ($count as $k => $v) {
+            if (!isset($pool[$data['處理單位名稱警局層']][$k])) {
+                $pool[$data['處理單位名稱警局層']][$k] = 0;
+            }
+            if (!isset($pool['total'][$k])) {
+                $pool['total'][$k] = 0;
+            }
+            $pool[$data['處理單位名稱警局層']][$k] += $v;
+            $pool['total'][$k] += $v;
+        }
     }
 }
-$fh = fopen($basePath . '/data/a2.csv', 'r');
+
+$fh = fopen($basePath . '/data/2023/a2.csv', 'r');
 $head = fgetcsv($fh, 2048);
-while($line = fgetcsv($fh, 2048)) {
-    $parts = explode(';', $line[2]);
-    foreach($parts AS $part) {
-        $d = preg_split('/([0-9]+)/', $part, -1, PREG_SPLIT_DELIM_CAPTURE);
-        if(!isset($count[$d[0]])) {
-            $count[$d[0]] = 0;
+while ($line = fgetcsv($fh, 2048)) {
+    $data = array_combine($head, $line);
+    if ($data['當事者順位'] == 1) {
+        $parts = explode(';', $data['死亡受傷人數']);
+        $count = [];
+        foreach ($parts as $v) {
+            $count[mb_substr($v, 0, 2, 'utf-8')] = intval(mb_substr($v, 2, null, 'utf-8'));
         }
-        $count[$d[0]] += $d[1];
-    }
-    $parts = explode(';', $line[3]);
-    foreach($parts AS $part) {
-        if(!isset($vehicles[$part])) {
-            $vehicles[$part] = 0;
+        if (!isset($pool[$data['處理單位名稱警局層']])) {
+            $pool[$data['處理單位名稱警局層']] = [];
         }
-        ++$vehicles[$part];
+        foreach ($count as $k => $v) {
+            if (!isset($pool[$data['處理單位名稱警局層']][$k])) {
+                $pool[$data['處理單位名稱警局層']][$k] = 0;
+            }
+            $pool[$data['處理單位名稱警局層']][$k] += $v;
+        }
     }
 }
-arsort($vehicles);
-print_r($vehicles);
+print_r($pool);
