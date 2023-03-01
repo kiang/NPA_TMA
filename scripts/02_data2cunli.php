1581a5a47c13e346adce3fb1174ce958297a4bfc
kiang <kiange@gmail.com>
2023-03-01 22:36:24 +0800

    update cunli data

--- scripts/02_data2cunli.php
+++ scripts/02_data2cunli.php
@@ -3,14 +3,68 @@
 $basePath = dirname(__DIR__);
 $config = require __DIR__ . '/config.php';
 $conn = new PDO('pgsql:host=localhost;dbname=' . $config['db'], $config['user'], $config['password']);
-$a1 = json_decode(file_get_contents($basePath . '/raw/a1.json'), true);
+$currentYear = date('Y');
 $cunli = [];
 $cunliWeekly = [];
-$oFh = [];
-foreach ($a1['result']['records'] as $record) {
-    $sql = "SELECT villcode FROM {$config['table']} AS cunli WHERE ST_Intersects('SRID=4326;POINT({$record['經度']} {$record['緯度']})'::geometry, cunli.geom)";
+
+$a2File = $basePath . '/data/' . $currentYear . '/a2.csv';
+$zip = new ZipArchive;
+$zip->open($a2File);
+for ($i = 0; $i < $zip->numFiles; $i++) {
+    $filename = $zip->getNameIndex($i);
+    if (false !== strpos($filename, 'NPA_TMA2')) {
+        $fh = fopen('zip://' . $a2File . '#' . $filename, 'r');
+        $head = fgetcsv($fh, 4096);
+        while ($line = fgetcsv($fh, 4096)) {
+            $data = array_combine($head, $line);
+            if (empty($data['經度'])) {
+                continue;
+            }
+            $sql = "SELECT villcode FROM {$config['table']} AS cunli WHERE ST_Intersects('SRID=4326;POINT({$data['經度']} {$data['緯度']})'::geometry, cunli.geom)";
+            $rs = $conn->query($sql);
+            if ($rs) {
+                $row = $rs->fetch(PDO::FETCH_ASSOC);
+            }
+
+            if (isset($row['villcode'])) {
+                if (!isset($cunli[$row['villcode']])) {
+                    $cunli[$row['villcode']] = [
+                        'a1' => 0,
+                        'a2' => 0,
+                        'total' => 0,
+                    ];
+                    $cunliWeekly[$row['villcode']] = [];
+                }
+                $time = mktime(substr($data['發生時間'], 0, 2), substr($data['發生時間'], 2, 2), substr($data['發生時間'], 4, 2), substr($data['發生日期'], 4, 2), substr($data['發生日期'], 6, 2), substr($data['發生日期'], 0, 4));
+                $week = date('oW', $time);
+                if (!isset($cunliWeekly[$row['villcode']][$week])) {
+                    $cunliWeekly[$row['villcode']][$week] = [
+                        'a1' => 0,
+                        'a2' => 0,
+                    ];
+                }
+
+                ++$cunli[$row['villcode']]['a2'];
+                ++$cunli[$row['villcode']]['total'];
+                ++$cunliWeekly[$row['villcode']][$week]['a2'];
+            }
+        }
+    }
+}
+
+$fh = fopen($basePath . '/data/' . $currentYear . '/a1.csv', 'r');
+$head = fgetcsv($fh, 4096);
+while ($line = fgetcsv($fh, 4096)) {
+    $data = array_combine($head, $line);
+    if (empty($data['經度'])) {
+        continue;
+    }
+    $sql = "SELECT villcode FROM {$config['table']} AS cunli WHERE ST_Intersects('SRID=4326;POINT({$data['經度']} {$data['緯度']})'::geometry, cunli.geom)";
     $rs = $conn->query($sql);
-    $row = $rs->fetch(PDO::FETCH_ASSOC);
+    if ($rs) {
+        $row = $rs->fetch(PDO::FETCH_ASSOC);
+    }
+
     if (isset($row['villcode'])) {
         if (!isset($cunli[$row['villcode']])) {
             $cunli[$row['villcode']] = [
@@ -20,8 +74,7 @@ foreach ($a1['result']['records'] as $record) {
             ];
             $cunliWeekly[$row['villcode']] = [];
         }
-        $time = preg_split('/[^0-9]+/i', $record['發生時間']);
-        $time = mktime($time[3], $time[4], $time[5], $time[1], $time[2], $time[0] += 1911);
+        $time = mktime(substr($data['發生時間'], 0, 2), substr($data['發生時間'], 2, 2), substr($data['發生時間'], 4, 2), substr($data['發生日期'], 4, 2), substr($data['發生日期'], 6, 2), substr($data['發生日期'], 0, 4));
         $week = date('oW', $time);
         if (!isset($cunliWeekly[$row['villcode']][$week])) {
             $cunliWeekly[$row['villcode']][$week] = [
@@ -36,38 +89,9 @@ foreach ($a1['result']['records'] as $record) {
     }
 }
 
-$a2 = json_decode(file_get_contents($basePath . '/raw/a2.json'), true);
-foreach ($a2['result']['records'] as $record) {
-    $sql = "SELECT villcode FROM {$config['table']} AS cunli WHERE ST_Intersects('SRID=4326;POINT({$record['經度']} {$record['緯度']})'::geometry, cunli.geom)";
-    $rs = $conn->query($sql);
-    $row = $rs->fetch(PDO::FETCH_ASSOC);
-    if (isset($row['villcode'])) {
-        if (!isset($cunli[$row['villcode']])) {
-            $cunli[$row['villcode']] = [
-                'a1' => 0,
-                'a2' => 0,
-                'total' => 0,
-            ];
-            $cunliWeekly[$row['villcode']] = [];
-        }
-        $time = preg_split('/[^0-9]+/i', $record['發生時間']);
-        $time = mktime($time[3], $time[4], $time[5], $time[1], $time[2], $time[0] += 1911);
-        $week = date('oW', $time);
-        if (!isset($cunliWeekly[$row['villcode']][$week])) {
-            $cunliWeekly[$row['villcode']][$week] = [
-                'a1' => 0,
-                'a2' => 0,
-            ];
-        }
-
-        ++$cunli[$row['villcode']]['a2'];
-        ++$cunli[$row['villcode']]['total'];
-        ++$cunliWeekly[$row['villcode']][$week]['a2'];
-    }
-}
-
 file_put_contents($basePath . '/data/cunli.json', json_encode($cunli, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 
+$oFh = [];
 foreach ($cunliWeekly as $villcode => $lv1) {
     $cunliPath = $basePath . '/data/cunli/' . substr($villcode, 0, 5);
     if (!file_exists($cunliPath)) {
