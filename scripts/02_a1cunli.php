cebfb1323aeb98034de59cfd3065e0781ff1fe77
kiang <kiange@gmail.com>
2024-03-05 14:53:04 +0800

    load old data to prevent duplicated job

--- scripts/02_a1cunli.php
+++ scripts/02_a1cunli.php
@@ -4,9 +4,20 @@ $basePath = dirname(__DIR__);
 $config = require __DIR__ . '/config.php';
 $conn = new PDO('pgsql:host=localhost;dbname=' . $config['db'], $config['user'], $config['password']);
 
+$header = ['villcode', '經度', '緯度'];
+$base = [];
 for ($i = 2018; $i <= 2024; ++$i) {
-    $oFh = fopen($basePath . "/data/cunli_lnglat/{$i}.csv", 'w');
-    fputcsv($oFh, ['villcode', '經度', '緯度']);
+    $csvFile = $basePath . "/data/cunli_lnglat/{$i}.csv";
+    if (file_exists($csvFile)) {
+        $fh = fopen($csvFile, 'r');
+        fgetcsv($fh, 2048);
+        while ($line = fgetcsv($fh, 2048)) {
+            $data = array_combine($header, $line);
+            $base["{$data['經度']} {$data['緯度']}"] = $data['villcode'];
+        }
+    }
+    $oFh = fopen($csvFile, 'w');
+    fputcsv($oFh, $header);
     $pool = [];
     foreach (glob($basePath . '/data/' . $i . '/*.csv') as $csvFile) {
         $fh = fopen($csvFile, 'r');
@@ -17,14 +28,19 @@ for ($i = 2018; $i <= 2024; ++$i) {
             if (empty($data['經度']) || isset($pool[$lnglat])) {
                 continue;
             }
-            $pool[$lnglat] = true;
-            $sql = "SELECT villcode FROM {$config['table']} AS cunli WHERE ST_Intersects('SRID=4326;POINT({$lnglat})'::geometry, cunli.geom)";
-            $rs = $conn->query($sql);
-            if ($rs) {
-                $row = $rs->fetch(PDO::FETCH_ASSOC);
-            }
-            if (!empty($row['villcode'])) {
-                fputcsv($oFh, [$row['villcode'], $data['經度'], $data['緯度']]);
+            if (isset($base[$lnglat])) {
+                $pool[$lnglat] = true;
+                fputcsv($oFh, [$base[$lnglat], $data['經度'], $data['緯度']]);
+            } else {
+                $sql = "SELECT villcode FROM {$config['table']} AS cunli WHERE ST_Intersects('SRID=4326;POINT({$lnglat})'::geometry, cunli.geom)";
+                $rs = $conn->query($sql);
+                if ($rs) {
+                    $row = $rs->fetch(PDO::FETCH_ASSOC);
+                }
+                if (!empty($row['villcode'])) {
+                    $pool[$lnglat] = true;
+                    fputcsv($oFh, [$row['villcode'], $data['經度'], $data['緯度']]);
+                }
             }
         }
     }
